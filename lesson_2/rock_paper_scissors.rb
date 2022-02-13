5c3a1a5afb3713115d7a684123a514cbcf7447cf
emmanuelkoh <nuelkoh@gmail.com>
2022-02-13 15:52:00 -0500

    added rock_paper_scissors.rb

---
+++ lesson_2/rock_paper_scissors.rb
@@ -0,0 +1,52 @@
+VALID_CHOICES = %w(rock paper scissors)
+
+def test_method
+  prompt 'test message'
+end
+
+def win?(first, second)
+  if (first == 'rock' && second == 'scissors') ||
+     (first == 'paper' && second == 'rock') ||
+     (first == 'scissors' && second == 'paper')
+  end
+end
+
+def display_result(player, computer)
+  if win?(player, computer)
+    prompt "You won!"
+  elsif win?(computer, player)
+    prompt "Computer won!"
+  else
+    prompt "It's a draw!"
+  end
+end
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  puts "You chose #{choice}; Computer chose: #{computer_choice}"
+
+  display_result(choice, computer_choice)
+
+  prompt "Do you want to play again?"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "That you for playing. Good bye!"
