7f6be8d093427ce9311d9f17981efdb69904d1a8
emmanuelkoh <nuelkoh@gmail.com>
2022-02-15 00:48:43 -0500

    finished lesson 3

---
+++ lesson_3/practice_problems_medium1/question_3.rb
@@ -0,0 +1,13 @@
+def factors(number)
+  divisor = number
+  factors = []
+  while divisor > 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+  factors
+end
+
+number = gets.chomp.to_i
+
+puts factors(number)
